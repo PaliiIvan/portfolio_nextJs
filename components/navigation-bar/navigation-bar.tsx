df947b6e05cbf27d9b2f49120cdc485170f0eb75
PaliiIvan <palii.ivan.v@gmail.com>
2022-08-24 14:34:01 +0300

    added new pointer

--- components/navigation-bar/navigation-bar.tsx
+++ components/navigation-bar/navigation-bar.tsx
@@ -1,34 +1,35 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useCallback, useState } from 'react';
-import { PAGES } from '../../helpers/constants/enums';
-import style from '../navigation-bar/navigation-bar.module.scss';
-import { NavItem } from '../navigation-item/navigation-item';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import { PAGES } from "../../helpers/constants/enums";
+import style from "../navigation-bar/navigation-bar.module.scss";
+import { NavItem } from "../navigation-item/navigation-item";
 
 export const NavigationBar = () => {
-    const router = useRouter();
-    const [, currentPage] = router.pathname.split('/');
+  const router = useRouter();
+  const [, currentPage] = router.pathname.split("/");
 
-    const gotTo = useCallback((page: PAGES) => () => {
-        router.push(page);
-    }, [router]);
+  const gotTo = useCallback(
+    (page: PAGES) => () => {
+      router.push(page);
+    },
+    [router]
+  );
 
-    return (
-        <div className={style.navigation_bar}>
-            <div className={style.initials}>
-                IP
-            </div>
-            <div className={style.nav_items_container}>
-                <NavItem onClick={gotTo(PAGES.ABOUT)} isActive={PAGES.ABOUT === currentPage}>
-                    About
-                </NavItem>
-                <NavItem onClick={gotTo(PAGES.SKILLS)} isActive={PAGES.SKILLS === currentPage}>
-                    Skills
-                </NavItem>
-                <NavItem onClick={gotTo(PAGES.EXPERIENCE)} isActive={PAGES.EXPERIENCE === currentPage}>
-                    Experience
-                </NavItem>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={style.navigation_bar}>
+      <div className={style.initials}>IP</div>
+      <div className={style.nav_items_container}>
+        <NavItem onClick={gotTo(PAGES.ABOUT)} isActive={PAGES.ABOUT === currentPage}>
+          About
+        </NavItem>
+        <NavItem onClick={gotTo(PAGES.SKILLS)} isActive={PAGES.SKILLS === currentPage}>
+          Skills
+        </NavItem>
+        <NavItem onClick={gotTo(PAGES.EXPERIENCE)} isActive={PAGES.EXPERIENCE === currentPage}>
+          Experience
+        </NavItem>
+      </div>
+    </div>
+  );
+};
--- components/page-wrapper/page-wrapper.tsx
+++ components/page-wrapper/page-wrapper.tsx
@@ -1,15 +1,41 @@
-import React, { FC } from "react"
-import { WithChildren } from "../../helpers/types/functional-component.type"
-import { NavigationBar } from "../navigation-bar/navigation-bar";
+import React, { FC, useEffect, useRef } from 'react';
+import { WithChildren } from '../../helpers/types/functional-component.type';
+import { NavigationBar } from '../navigation-bar/navigation-bar';
 import style from './page-wrapper.module.scss';
 
 const PageWrapper: FC<WithChildren> = ({ children }) => {
-    return (
-        <div className={style.page_wrapper}>
-            <NavigationBar />
-            {children}
-        </div>)
-};
+  const pointerRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    const pointerDiv = pointerRef.current;
+
+    if (!pointerDiv) {
+      return;
+    }
+
+    const { width } = pointerDiv.getBoundingClientRect();
+    const difSize = width / 2;
+    const mouseMoveTrack = ({ clientX, clientY }: MouseEvent) => {
+      pointerDiv.style.transform = `translate(${clientX - difSize}px, ${clientY - difSize}px)`;
+    };
+
+    if (pointerDiv) {
+      window.addEventListener('mousemove', mouseMoveTrack);
+    }
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveTrack);
+    };
+  }, []);
+
+  return (
+    <div className={style.page_wrapper}>
+      <div ref={pointerRef} className={style.pointer}>
+        <div className={style.inner_circle}></div>
+      </div>
+      <NavigationBar />
+      {children}
+    </div>
+  );
+};
 
 export default PageWrapper;
